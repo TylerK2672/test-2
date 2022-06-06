a0bab20f1bc9890270deb491d8ef69b82acaa58c
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-06-05 23:25:14 -0600

    orders completed

--- orders.js
+++ orders.js
@@ -29,7 +29,22 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket
+{
+    constructor(items, orderTime, customerId)
+    {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = 'queued'
+    }
+
+    updateStatus(newStatus)
+    {
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 
@@ -44,7 +59,8 @@
     customer: 575
 */
 
-//CODE HERE
+const firstTicket = new Ticket(['pizza', 'bread', 'soda'], "7:03 PM", 575)
+console.log(firstTicket)
 
 
 /*
@@ -53,4 +69,5 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus("Cooking")
+console.log(firstTicket)
